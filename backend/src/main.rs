6c076567979e93748f13600f45f589ec8092b241
Anirudh Valiveru <anirudhv@mit.edu>
2026-03-07 17:02:38 -0800

    feat: added CLI argument parsing

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,10 @@
+use std::path::PathBuf;
+
 use axum::Router;
+use clap::Parser;
 use dotenvy::dotenv;
 use tracing::info;
+use tracing_subscriber::EnvFilter;
 
 use crate::{
     controllers::{health, redis},
@@ -12,18 +16,37 @@ mod controllers;
 mod error;
 mod state;
 
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct CLIArguments {
+    #[arg(long)]
+    redis_socket: PathBuf,
+}
+
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> AppResult<()> {
     dotenv().ok();
-    env_logger::init();
+    tracing_subscriber::fmt()
+        .compact()
+        .without_time()
+        .with_file(true)
+        .with_line_number(true)
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
 
-    let app = create_app().with_state(AppState);
+    let args = CLIArguments::parse();
+    let app = create_app(args);
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
     info!("🚀 server listening on 127.0.0.1:3000");
     axum::serve(listener, app).await?;
     Ok(())
 }
 
-pub fn create_app() -> Router<AppState> {
-    Router::new().merge(health::router()).merge(redis::router())
+fn create_app(args: CLIArguments) -> Router {
+    Router::new()
+        .merge(health::router())
+        .merge(redis::router())
+        .with_state(AppState {
+            redis_socket: args.redis_socket,
+        })
 }
--- backend/src/state.rs
+++ backend/src/state.rs
@@ -1,2 +1,9 @@
+use std::path::PathBuf;
+
 #[derive(Clone)]
-pub struct AppState;
+pub struct AppState {
+    /// The path to the Unix domain socket
+    /// file the Redis server is listening
+    /// on.
+    pub redis_socket: PathBuf,
+}
